6a3b9b5d1103a0b3052af207855b22b460a8568d
Himadhar H <misterhimadhar@gmail.com>
2023-10-04 18:19:30 +0200

    updated button
    updated button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,8 +4,8 @@ export type FButtonProps = {
   label: string;
 };
 
-const Button: FC<FButtonProps> = (props: FButtonProps) => {
+const FormButton: FC<FButtonProps> = (props: FButtonProps) => {
   return <h1>{props.label}</h1>;
 };
 
-export default Button;
+export default FormButton;
